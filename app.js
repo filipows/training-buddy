a54bc8d4d8a34098821ccf8c653ab9a779175af7
filipows <3.3fullpower@gmail.com>
2015-09-05 19:38:20 +0200

    Add public: bower, angular sample views.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var jadeStatic = require('connect-jade-static');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,9 +20,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(jadeStatic({
+  baseDir: path.join(__dirname, '/public'),
+  baseUrl: '/',
+  maxAge: 86400,
+  jade: { pretty: true }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
