743c1db4068c2c95a4b538c97ff69d09d3652563
filipows <3.3fullpower@gmail.com>
2015-09-20 22:50:53 +0200

    Basic statistics table

--- public/src/statistics/statistics.js
+++ public/src/statistics/statistics.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var apiUrl = '/api/v1'
+
 angular.module('trainingBuddy.statisticsView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +11,13 @@ angular.module('trainingBuddy.statisticsView', ['ngRoute'])
   });
 }])
 
-.controller('StatisticsCtrl', [function() {
-
-}]);
+.controller('StatisticsCtrl', function($scope, $http) {
+  $http.get(apiUrl + '/exercises').then(
+    function(response) {
+      $scope.exercises = response.data;
+    },
+    function(error){
+      console.log(error);
+    }
+  );
+});
